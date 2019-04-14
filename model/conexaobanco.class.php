69dcbac46a230a66c601e777122b31e332e5ac0a
Giovane Garcia <giovane.garcia09@gmail.com>
2019-04-14 19:04:53 -0300

    Update conexaobanco.class.php

--- model/conexaobanco.class.php
+++ model/conexaobanco.class.php
@@ -9,7 +9,7 @@ class ConexaoBanco extends PDO{
     public static function getInstance() {
         try {
             if (!isset(self::$instance)) {
-                self::$instance = new PDO('mysql:host=localhost;dbname=meu_mercado', 'root', '');
+                self::$instance = new PDO('mysql:host=mysql995.umbler.com;dbname=meu_mercado', 'giovanegarcia', 'testesoftware');
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                
             }//fecha if
             return self::$instance;
