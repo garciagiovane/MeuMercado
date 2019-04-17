e71efbbe7e0b50c6671ce27763bd38ac513ba4c6
GIOVANE DE GARCIA PACHECO <ALU201819089@esade.local>
2019-04-17 10:00:49 -0300

    nada

--- model/conexaobanco.class.php
+++ model/conexaobanco.class.php
@@ -9,13 +9,13 @@ class ConexaoBanco extends PDO{
     public static function getInstance() {
         try {
             if (!isset(self::$instance)) {
-                self::$instance = new PDO('mysql:host=mysql995.umbler.com;dbname=meu_mercado', 'giovanegarcia', 'testesoftware');
+                self::$instance = new PDO('mysql:host=127.0.0.1;dbname=meu_mercado', 'root', '');
+                //self::$instance = new PDO('mysql:host=mysql995.umbler.com;dbname=meu_mercado', 'giovanegarcia', 'testesoftware');
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                
-            }//fecha if
+            }
             return self::$instance;
         } catch (Exception $exc) {
-            $_SESSION["erroConexao"] = $exc->getMessage();
-            
+            $_SESSION["erroConexao"] = $exc->getMessage();            
             $location = "Location: ../view/resposta.php";
             header($location);
         }
