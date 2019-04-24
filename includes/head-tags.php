01f5890f80084d9003a56e6593e455b67ba6566a
GIOVANE DE GARCIA PACHECO <ALU201819089@esade.local>
2019-04-24 11:36:38 -0300

    vai incluir lista com sort

--- includes/head-tags.php
+++ includes/head-tags.php
@@ -9,15 +9,37 @@
     <meta name="description" content="Escolha os produtos que quer no conforto de sua casa">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv=”content-type” content=”text/html; charset=UTF-8″ />
-    <link rel="stylesheet" href="../style/bootstrap.min.css">
-    <script src="../style/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script type="text/javascript" src="../style/datatables.min.js"></script> -->
+    <script type="text/javascript" src="../style/jquery.tablesorter.js"></script>
+    <script type="text/javascript" src="../style/jquery.tablesorter.widgets.js"></script>
+    <script>
+        $(function() {
+            $("#dtHorizontalExample").tablesorter();
+        });
+        /*
+        $(document).ready(function() {
+            $('#dtHorizontalExample').DataTable();
+            $('.dataTables_length').addClass('bs-select');
+        });*/
+    </script>
     <style>
-        #iconeExclusao, #iconeAlteracao, #iconeCompra {
+        /*.row {
+            width: 100%;
+
+        }*/
+
+        #iconeExclusao,
+        #iconeAlteracao,
+        #iconeCompra {
             width: 30px;
             height: 30px;
         }
-        @media (max-width: 991.98px) { 
-            .container{
+
+        @media (max-width: 991.98px) {
+            .container {
                 width: 100%;
                 padding: 3px;
             }
@@ -28,8 +50,8 @@
             }
         }
 
-        @media (max-width: 767.98px) { 
-            .container{
+        @media (max-width: 767.98px) {
+            .container {
                 width: 100%;
                 padding: 3px;
             }
